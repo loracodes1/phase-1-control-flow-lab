6e6c4b481040475ac79e1223a14ad902d9dc8445
florence <loracodes3@gmail.com>
2024-10-01 07:35:00 +0300

    commit

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ function scuberGreetingForFeet(feet){
   // Write your code here!
   if (feet<=400){
     return ' This one is on me!'
-  }elseif(feet > 400 && feet =< 2000);
+  }elseif(feet >=400 && feet<=2000);
   {
     return "That will be twenty bucks."
+  }
+  elseif (feet > 2000 && feet <= 2500) ;{
+    return 'I will gladly take your thirty bucks.';
+  } ;
+  elseif (feet >2500)
+  {
+    return 'No can do.';
   }
 }
 
