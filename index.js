8d2ed44fb5decbed10327b5e48f53c3e9e75aecc
florence <loracodes3@gmail.com>
2024-10-01 07:21:34 +0300

    if elseif with a function

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if (feet<=400){
+    return ' This one is on me!'
+  }elseif(feet > 400 && feet =< 2000);
+  {
+    return "That will be twenty bucks."
+  }
 }
 
 function ternaryCheckCity(){
