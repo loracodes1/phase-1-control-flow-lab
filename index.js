9c6785724ad143487edc32cc579d20ec552c5308
florence <loracodes3@gmail.com>
2024-10-01 07:55:47 +0300

    commit

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 function scuberGreetingForFeet(feet){
   // Write your code here!
-  if (feet<=400){
-    return ' This one is on me!'
-  }elseif(feet >=400 && feet<=2000);
-  {
-    return "That will be twenty bucks."
+    if (feet <= 400) {
+      return 'This one is on me!';
+    } else if (feet > 400 && feet <= 2000) {
+      return 'That will be twenty bucks.';
+    } else if (feet > 2000 && feet <= 2500) {
+      return 'I will gladly take your thirty bucks.';
+    } else {
+      return 'No can do.';
+    }
   }
-  elseif (feet > 2000 && feet <= 2500) ;{
-    return 'I will gladly take your thirty bucks.';
-  } ;
-  elseif (feet >2500)
-  {
-    return 'No can do.';
-  }
-}
+  
 
 function ternaryCheckCity(City){
 City===NYC ? 'Ok, sounds good.' : "No go."
